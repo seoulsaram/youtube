4061acec30e43e6c13feee1b70334ceeed445458
seoulsaram <kaste2233@gmail.com>
2021-02-04 03:06:50 +0900

    add video detail

--- src/app.jsx
+++ src/app.jsx
@@ -3,13 +3,23 @@ import styles from "./app.module.css";
 import VideoList from "./components/video_list/video_list";
 import "@fortawesome/fontawesome-free/js/all.js";
 import SearchHeader from "./components/search_header/search_header";
+import VideoDetail from "./components/video_detail/video_detail";
 
 function App({ youtube }) {
   const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
+  const selectVideo = (videos) => {
+    setSelectedVideo(videos);
+  };
+
   const search = (query) => {
     youtube
       .search(query) //
-      .then((videos) => setVideos(videos));
+      .then((videos) => {
+        setVideos(videos);
+        setSelectedVideo(null);
+      });
   };
 
   useEffect(() => {
@@ -17,16 +27,29 @@ function App({ youtube }) {
       .mostPopular() //
       .then((videos) => setVideos(videos));
   }, []);
-
   //마운트가 되었거나 업데이트가 되었을 때 콜백을 등록할 수 있는게 useEffect()
   //콜백 뒤에 인자를 보내면, 해당 인자가 업데이트 되었을 경우에만 콜백함수가 실행된다.
   //텅텅 빈 인자를 넣어두면, 마운트가 되었을 때만 호출됨.
   //아무것도 전달하지 않으면 어떤 컴포넌트나 props가 업데이트 되든 호출되므로 비효율적이다.
   //<VideoList videos={videos} />
+
   return (
     <div className={styles.app}>
       <SearchHeader onSearch={search} />
-      <VideoList videos={videos} />
+      <section className={styles.content}>
+        {selectedVideo && (
+          <div className={styles.detail}>
+            <VideoDetail video={selectedVideo} />
+          </div>
+        )}
+        <div className={styles.list}>
+          <VideoList
+            videos={videos}
+            onVideoClick={selectVideo}
+            display={selectedVideo ? "list" : "grid"}
+          />
+        </div>
+      </section>
     </div>
   );
 }
--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,24 +1,31 @@
 import React from "react";
 import styles from "./video_item.module.css";
 
-const VideoItem = ({ video: { snippet } }) => (
-  //const VideoItem = (props) => props.video~ 이렇게 쓰는게 싫으면
-  //const VideoItem = ({video}) => 처음부터 video를 이렇게 받아와도 되고
-  //const VideoItem = ({video : videoItem}) => 받아온 video의 이름을 지정하고 싶을 땐 이렇게 해도 됨
-  //const VideoItem = ({video: {snippet}}) 이건 video 안에 있는 snippet을 가져오겠다는 것
-  <li className={styles.container}>
-    <div className={styles.video}>
-      <img
-        className={styles.thumbnail}
-        src={snippet.thumbnails.medium.url}
-        alt="video thumbnail"
-      />
-      <div className={styles.metadata}>
-        <p className={styles.title}>{snippet.title}</p>
-        <p className={styles.channel}>{snippet.channelTitle}</p>
+const VideoItem = ({ video, video: { snippet }, onVideoClick, display }) => {
+  const displayType = display === "list" ? styles.list : styles.grid;
+  //display가 list면 styles의 list를 쓰고, 아니면 styles에 있는 grid를 써
+  return (
+    //const VideoItem = (props) => props.video~ 이렇게 쓰는게 싫으면
+    //const VideoItem = ({video}) => 처음부터 video를 이렇게 받아와도 되고
+    //const VideoItem = ({video : videoItem}) => 받아온 video의 이름을 지정하고 싶을 땐 이렇게 해도 됨
+    //const VideoItem = ({video: {snippet}}) 이건 video 안에 있는 snippet을 가져오겠다는 것
+    <li
+      className={`${styles.container} ${displayType}`}
+      onClick={() => onVideoClick(video)}
+    >
+      <div className={styles.video}>
+        <img
+          className={styles.thumbnail}
+          src={snippet.thumbnails.medium.url}
+          alt="video thumbnail"
+        />
+        <div className={styles.metadata}>
+          <p className={styles.title}>{snippet.title}</p>
+          <p className={styles.channel}>{snippet.channelTitle}</p>
+        </div>
       </div>
-    </div>
-  </li>
-);
+    </li>
+  );
+};
 
 export default VideoItem;
